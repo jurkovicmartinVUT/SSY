754932c1b857c08434575032c516e1f0b8b25739
jurkovicmartinVUT <124887720+jurkovicmartinVUT@users.noreply.github.com>
2025-04-07 15:01:21 +0200

    Add files via upload

---
+++ Project2/LWM_MSSY/Debug/stack/mqtt/mqtt_interface.d
@@ -0,0 +1,78 @@
+stack/mqtt/mqtt_interface.d stack/mqtt/mqtt_interface.o: \
+ ../stack/mqtt/mqtt_interface.c ../stack/mqtt/mqtt_interface.h \
+ c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ ../stack/mqtt/../../globals.h \
+ c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\iom256rfr2.h \
+ c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h \
+ c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\wdt.h \
+ ../stack/mqtt/../../stack/eth/socket.h \
+ ../stack/mqtt/../../stack/eth/wizchip_conf.h \
+ ../stack/mqtt/../../stack/eth/W5500/w5500.h \
+ ../stack/mqtt/../../stack/eth/W5500/../wizchip_conf.h \
+ ../stack/mqtt/../../stack/eth/wizchip_conf.h \
+ ../stack/mqtt/../eth/wizchip_conf.h ../stack/mqtt/../eth/socket.h
+
+../stack/mqtt/mqtt_interface.h:
+
+c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+../stack/mqtt/../../globals.h:
+
+c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\iom256rfr2.h:
+
+c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h:
+
+c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\wdt.h:
+
+../stack/mqtt/../../stack/eth/socket.h:
+
+../stack/mqtt/../../stack/eth/wizchip_conf.h:
+
+../stack/mqtt/../../stack/eth/W5500/w5500.h:
+
+../stack/mqtt/../../stack/eth/W5500/../wizchip_conf.h:
+
+../stack/mqtt/../../stack/eth/wizchip_conf.h:
+
+../stack/mqtt/../eth/wizchip_conf.h:
+
+../stack/mqtt/../eth/socket.h:
---
+++ Project2/LWM_MSSY/Debug/stack/mqtt/packet/src/MQTTConnectServer.d
@@ -0,0 +1,45 @@
+stack/mqtt/packet/src/MQTTConnectServer.d \
+ stack/mqtt/packet/src/MQTTConnectServer.o: \
+ ../stack/mqtt/packet/src/MQTTConnectServer.c \
+ ../stack/mqtt/packet/src/StackTrace.h \
+ c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
+ c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
+ c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ ../stack/mqtt/packet/src/MQTTPacket.h \
+ ../stack/mqtt/packet/src/MQTTConnect.h \
+ ../stack/mqtt/packet/src/MQTTPublish.h \
+ ../stack/mqtt/packet/src/MQTTSubscribe.h \
+ ../stack/mqtt/packet/src/MQTTUnsubscribe.h \
+ ../stack/mqtt/packet/src/MQTTFormat.h \
+ c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h
+
+../stack/mqtt/packet/src/StackTrace.h:
+
+c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
+
+c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
+
+c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+../stack/mqtt/packet/src/MQTTPacket.h:
+
+../stack/mqtt/packet/src/MQTTConnect.h:
+
+../stack/mqtt/packet/src/MQTTPublish.h:
+
+../stack/mqtt/packet/src/MQTTSubscribe.h:
+
+../stack/mqtt/packet/src/MQTTUnsubscribe.h:
+
+../stack/mqtt/packet/src/MQTTFormat.h:
+
+c:\apps\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h:
